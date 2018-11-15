b8ea83c4b2524545fbf9c9d2a2347ca70d46e30d
Tom <thomaswarne@gmail.com>
2018-11-15 05:33:17 -0800

    Create controller scaffolding

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,18 @@
+class ListsController < ApplicationController
+
+  def add_task
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+end
---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,14 @@
+class TasksController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+end
