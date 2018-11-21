59382c1555abde4ffd5283c446bded3d3f4725d9
Tom <thomaswarne@gmail.com>
2018-11-21 15:15:21 -0800

    OMG you cannot have the scss pack and js pack named the same thing.  I swear it was working yesterday.  There goes 2 hours of my life...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
-  
+
   def query(q)
     ActiveRecord::Base.connection.exec_query(q)
   end
