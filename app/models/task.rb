95e32ffbc54a687396638ec695796287aff9dd7a
Tom <thomaswarne@gmail.com>
2018-11-27 09:09:08 -0800

    Fix List deletion relation bug

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :list
 
-  has_many :tag_tasks
-  has_many :tags, through: :tag_tasks
+  has_many :tag_tasks, dependent: :destroy
+  has_many :tags,      through: :tag_tasks
 
   validates :title, presence: true
 end
